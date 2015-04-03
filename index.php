9b88ff73f7f0ca0e87bd9bb69ff2c17e484bee60
Daniel Andrade <daniel@danielandrade.net>
2015-04-03 14:39:47 +1100

    Add footer.

--- index.php
+++ index.php
@@ -3,8 +3,53 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Daniel Andrade</title>
+
+	<!--styles-->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	
+	<link href="css/style.css?v=1" rel="stylesheet">
+
 </head>
 <body>
 	
+	<section class="contact">
+		<div class="container">
+			<div class="row">
+				<div class="sign-up">
+					<h2 class="logo-contact"></h2>
+					<h2 class="contact_txt">Get in touch!</h2>						
+					<p class="contact_txt">I'm available for freelance contracts, so get in touch: <br/>
+						<SCRIPT TYPE="text/javascript">
+							emailE=('daniel@' + 'danielandrade.net')	
+							document.write('<a href="mailto:' + emailE + '">' + emailE + '</a>')
+						</SCRIPT></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer id="footer">
+		<div class="row">
+			<div class="col-md-4">
+				<ul class="footer-icons">
+					<li><a href="https://github.com/dansku" target="_blank"><i class="fa fa-github-square fa-3" style="font-size:42px;"></i></a></li>
+					<li><a href="https://www.linkedin.com/in/dansku" target="_blank"><i class="fa fa-linkedin-square fa-3" style="font-size:42px;"></i></a></li>
+					<li><a href="https://instagram.com/danskubr" target="_blank"><i class="fa fa-instagram fa-3" style="font-size:42px;"></i></a></li>
+					<li><a href="https://www.youtube.com/danskubr" target="_blank"><i class="fa fa-youtube-square fa-3" style="font-size:42px;"></i></a></li>
+					<li><a href="https://twitter.com/dspillere" target="_blank"><i class="fa fa-twitter-square fa-3" style="font-size:42px;"></i></a></li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</footer>
+	
+	<!-- javascript -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.transit/0.9.12/jquery.transit.min.js"></script>
+	<script src="js/script.js"></script>
+
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function($) {
+	var angle = 45;
+	var duration = 200;
+
+	$( ".fa" ).mouseover(function() {var angle = Math.floor((Math.random() * 70) + 10);$(this).transition({ rotate: angle,  duration: duration });}).mouseleave(function() {$(this).transition({ rotate: 0,  duration: duration });});
+
+	$( ".logo-contact" ).mouseover(function() {
+		$(this).transition({ perspective: '100px',rotateY: '180deg'});
+		}).mouseleave(function() {
+			$(this).transition({ perspective: '100px',rotateY: '0deg'});
+		});
+});
